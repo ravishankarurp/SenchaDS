7e4d7a0ac15ba4b0bcdc390a9e8a7363b5926e40
Ravi Parupudi <ravishankarurp@gmail.com>
2013-02-09 21:09:13 +0000

    1

---
+++ client/default/app/view/Main.js
@@ -0,0 +1,36 @@
+SimpleApp.view.Main = Ext.define('SimpleApp.view.Main', {
+    extend: 'Ext.form.Panel',
+    xtype: 'main',
+    requires: ['Ext.form.FieldSet'],
+    config: {
+        url: 'contact.js',
+        fullscreen:true,
+        items: [
+            {
+                xtype: 'fieldset',
+				title: 'Contact Us',
+
+                items: [
+                    {
+                        xtype: 'textfield',
+                        name: 'name',
+                        label: 'Name'
+                    },
+                    {
+                        xtype: 'textfield',
+                        name: 'contactnumber',
+                        label: 'Contact Number'
+                    }
+                ]
+            },
+            {
+                xtype: 'button',
+                text: 'Submit',
+                ui: 'confirm',
+                handler: function(){
+                    this.up('main').submit();
+                }
+            }
+        ]
+    }
+});
